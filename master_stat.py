299154e82a237f419a7b760c1b86fcbb8c8892fb
Philipp Panhey <philipp.panhey@gmail.com>
2022-11-24 12:23:39 +0100

    wip

--- master_stat.py
+++ master_stat.py
@@ -1,5 +1,22 @@
+import argparse
+import pandas as pd
+
+
 def main():
-    print("Hello, World!")
+    args = parse_args()
+    df = import_csv_as_dataframe(args["csv_filename"])
+    print(df.loc[[0, 1, 2]])
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--csv_filename", default="data.csv")
+    return vars(parser.parse_args())
+
+
+def import_csv_as_dataframe(filename):
+    df = pd.read_csv(filename, encoding="utf-16", sep="\t")
+    return df
 
 
 if __name__ == "__main__":
